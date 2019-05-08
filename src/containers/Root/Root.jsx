6370153f092486a774f323c703ad2e645742e2ae
reaysawa <joao.paulo.silvasouza@hotmail.com>
2019-05-08 01:09:56 -0300

    no routing in the master branch

--- src/containers/Root/Root.jsx
+++ src/containers/Root/Root.jsx
@@ -1,25 +1,13 @@
 import React from "react"
-//import { hot } from "react-hot-loader/root"
-import { BrowserRouter, Switch, Route } from "react-router-dom"
-import { useLazyBundle } from 'src/utils/useLazyBundle'
-import LoadingComponent from 'src/components/Loading/Loading.jsx'
+import { hot } from "react-hot-loader/root"
+import { useLazyBundle } from "src/utils/useLazyBundle"
+import LoadingComponent from "src/components/Loading/Loading.jsx"
 import HomePageLazy from "bundle-loader?lazy!src/pages/HomePage"
-import AboutPageLazy from "bundle-loader?lazy!src/pages/AboutPage"
 
 const Root = () => {
   const [HomePage] = useLazyBundle(HomePageLazy, 500, LoadingComponent)
-  const [AboutPage] = useLazyBundle(AboutPageLazy, 500, LoadingComponent)
 
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/about" component={AboutPage} />
-        <Route exact path="/" component={HomePage} />
-      </Switch>
-    </BrowserRouter>
-  )
+  return <HomePage />
 }
 
-export default Root
-
-//export default hot(Root)
+export default hot(Root)
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,15 +5,12 @@ import MainLayout from "src/layouts/MainLayout/MainLayout.jsx"
 import HelloReactLazy from "bundle-loader?lazy!src/components/HelloReact/HelloReact.jsx"
 import LoadingComponent from 'src/components/Loading/Loading.jsx'
 
-import { Link } from 'react-router-dom'
-
 function HomePage() {
   const [HelloReact] = useLazyBundle(HelloReactLazy, 500, LoadingComponent)
 
   return (
     <MainLayout>
-      <Link to={{ pathname: '/about' }}>About!</Link>
-      <HelloReact name={"Charles"} />
+      <HelloReact name={"Michael"} />
     </MainLayout>
   )
 }
